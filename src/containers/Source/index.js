cc855a383ecd9caf525ec01200f9268fb347b857
Rahman <abdulrahman6093@outlook.com>
2024-04-24 01:08:13 +0530

    basic files

---
+++ src/containers/Source/index.js
@@ -0,0 +1,11 @@
+import styles from './Source.module.css';
+
+const Source = () => {
+  return (
+    <div className={styles.source}>
+      <h1>Source</h1>
+    </div>
+  );
+};
+
+export default Source;
