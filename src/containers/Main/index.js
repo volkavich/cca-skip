e9ee59d793b3955b64ae6ae7dc56f414a9571ca3
Rahman <abdulrahman6093@outlook.com>
2024-04-24 23:41:09 +0530

    get route

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from './Main.module.css';
 import Source from '../Source';
 import Destination from '../Destination';
-// import Route from '../Route';
+import Route from '../Route';
 
 const Home = () => {
   return (
     <div className={styles.main}>
       <Source />
       <Destination />
-      {/*<Route />*/}
+      <Route />
     </div>
   );
 };
--- src/containers/Route/index.js
+++ src/containers/Route/index.js
@@ -1,52 +1,50 @@
+import React from 'react';
 import styles from './Route.module.css';
-import { Button } from '@mui/material';
+import { Button, CircularProgress } from '@mui/material';
 import useRouteStore from '../../store/routeStore';
 import useSourceStore from '../../store/sourceStore';
 import useDestinationStore from '../../store/destinationStore';
 
 const Route = () => {
-  const { routeData, postRouteRequest } = useRouteStore();
+  const { routeData, postRouteRequest, postRouteInProgress, routeFound } = useRouteStore();
   const { sourceChain, sourceToken } = useSourceStore();
   const { destinationChain, destinationToken } = useDestinationStore();
 
   const handleClick = () => {
-    postRouteRequest(
-      sourceToken.denom,
-      sourceChain.chain_id,
-      destinationToken.denom,
-      destinationChain
-    );
+    if (
+      sourceToken.denom &&
+      sourceChain.chain_id &&
+      destinationToken.denom &&
+      destinationChain.chain_id
+    ) {
+      postRouteRequest(
+        sourceToken.denom,
+        sourceChain.chain_id,
+        destinationToken.denom,
+        destinationChain.chain_id
+      );
+    }
   };
+
+  const disable =
+    !sourceToken.denom ||
+    !sourceChain.chain_id ||
+    !destinationToken.denom ||
+    !destinationChain.chain_id;
 
   return (
     <div className={styles.route}>
-      {sourceToken && sourceChain && destinationToken && destinationChain ? (
-        <Button onClick={handleClick}>Get Route</Button>
-      ) : null}
-      {routeData && routeData.operations && routeData.operations[0].transfer ? (
-        <div className={styles.details}>
-          <p>
-            Bridge:{' '}
-            {routeData.operations[0].transfer.bridge_id &&
-              routeData.operations[0].transfer.bridge_id}
-          </p>
-          <p>
-            From:{' '}
-            {routeData.operations[0].transfer.bridge_id &&
-              routeData.operations[0].transfer.chain_id}
-          </p>
-          <p>
-            To:{' '}
-            {routeData.operations[0].transfer.bridge_id &&
-              routeData.operations[0].transfer.to_chain_id}
-          </p>
-          <p>
-            Port:{' '}
-            {routeData.operations[0].transfer.bridge_id &&
-              routeData.operations[0].transfer.port}
-          </p>
+      <Button disabled={disable} onClick={handleClick}>
+        {postRouteInProgress ? <CircularProgress /> : <p>Get Route</p>}
+      </Button>
+      {routeFound === 'true' && routeData && Object.keys(routeData).length ? (
+        <div className={styles.green}>
+          Route found with {routeData.operations && routeData.operations.length} operations and{' '}
+          {routeData.txs_required} <i>txs</i>
         </div>
-      ) : null}
+      ) : (
+        routeFound === 'error' && <div className={styles.red}>No Route found</div>
+      )}
     </div>
   );
 };
